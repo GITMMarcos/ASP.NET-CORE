96bec8eb002e4feaeaeef0759f547a24f148cdb0
mauriciomarcos.consultoria@gmail.com <mauriciomarcos.consultoria@gmail.com>
2018-12-02 15:47:50 -0200

    Criação da classe Startup.cs para inicializar o hosting.

---
+++ 03/ConsoleApp_ClasseDeInicializacao/ConsoleApp_ClasseDeInicializacao/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace ConsoleApp_ClasseDeInicializacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebHost host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+
+    //Foi isolada a responsabilidade.
+    //Toda aplicação Web posuirá uma classe Startup.cs com o método Configure(IApplicationBuilder app)
+    internal class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Use(async (context, next) => {
+                await context.Response.WriteAsync("Iniciando um hosting através da classe Startup.cs");
+            });
+        }
+    }
+}
