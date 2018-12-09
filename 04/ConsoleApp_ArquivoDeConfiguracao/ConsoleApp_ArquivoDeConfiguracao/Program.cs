5077faf4cf20feb84a26aca56e387706b98bc27e
mauriciomarcos.consultoria@gmail.com <mauriciomarcos.consultoria@gmail.com>
2018-12-09 00:28:22 -0200

    Arquivo de configuração no .NET CORE [appsettings.json]

--- 04/ConsoleApp_ArquivoDeConfiguracao/ConsoleApp_ArquivoDeConfiguracao/Program.cs
+++ 04/ConsoleApp_ArquivoDeConfiguracao/ConsoleApp_ArquivoDeConfiguracao/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 
 namespace ConsoleApp_ArquivoDeConfiguracao
 {
@@ -6,7 +10,31 @@ namespace ConsoleApp_ArquivoDeConfiguracao
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var host = WebHost
+                .CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
         }
+    }
+
+    internal class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Use(async (context, next) =>
+            {
+                await context.Response.WriteAsync(_configuration["name"]);
+            });
+        }
+        
     }
 }
